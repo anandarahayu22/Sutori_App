0ba9c592debdec9a29f33029dfe6aa4d35c2657f
anandarahayu22 <anandarahayu967@gmail.com>
2024-04-24 21:38:46 +0700

    List view dan layout builder

--- lib/add_story_screen.dart
+++ lib/add_story_screen.dart
@@ -1,13 +1,41 @@
 import 'package:flutter/material.dart';
 
 class AddStoryScreen extends StatelessWidget {
-  const AddStoryScreen({super.key});
+  const AddStoryScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Column(
-      children: [IconButton(onPressed: () {}, icon: Icon(Icons.home))],
-    ));
+      backgroundColor: Color.fromARGB(154, 161, 91, 41),
+      body: LayoutBuilder(
+        builder: (BuildContext context, constraints) {
+          if (constraints.maxWidth < 600) {
+            return ListView(
+              children: _generateConstraints(),
+            );
+          } else if (constraints.maxWidth < 900) {
+            return GridView.count(
+              crossAxisCount: 2,
+              children: _generateConstraints(),
+            );
+          } else {
+            return GridView.count(
+              crossAxisCount: 6,
+              children: _generateConstraints(),
+            );
+          }
+        },
+      ),
+    );
+  }
+
+  List<Widget> _generateConstraints() {
+    return List<Widget>.generate(10, (index) {
+      return Container(
+        margin: EdgeInsets.all(8),
+        color: Colors.brown,
+        height: 200,
+      );
+    });
   }
 }
--- lib/info_detail_screen.dart
+++ lib/info_detail_screen.dart
@@ -1,14 +1,32 @@
 import 'package:flutter/material.dart';
 
 class InfoDetailScreen extends StatelessWidget {
-  const InfoDetailScreen({super.key});
+  const InfoDetailScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    Size screenSize = MediaQuery.of(context).size;
+    Orientation screenOrientation = MediaQuery.of(context).orientation;
+
     return Scaffold(
-      appBar: AppBar(title: Text("TOTAL PESANAN")),
-      body: Center(
-        child: Text("TOTAL PESANAN"),
+      backgroundColor: Colors.brown,
+      body: SafeArea(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center, // perbaikan #1
+          crossAxisAlignment: CrossAxisAlignment.stretch, // perbaikan #1
+          children: [
+            Text(
+              'Screen Size : ${screenSize.width.toStringAsFixed(2)}',
+              style: TextStyle(color: Colors.white, fontSize: 18),
+              textAlign: TextAlign.center,
+            ),
+            Text(
+              'Screen Size : ${screenSize.width.toStringAsFixed(2)}',
+              style: TextStyle(color: Colors.white, fontSize: 18),
+              textAlign: TextAlign.center,
+            ),
+          ],
+        ),
       ),
     );
   }
